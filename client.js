1cb5a67a3c4e5c6a777d902bf5ac8468bc553b7f
vlad-senyuk-1997 <vladsenyuk1975@gmail.com>
2018-09-11 18:20:48 +0300

    task 4-2
    tak 4-2

--- client.js
+++ client.js
@@ -3,14 +3,32 @@ const net = require('net');
 const port = 8124;
 const client = new net.Socket();
 const requestString = '\r\nQA\r\n';
-let json = require("./qa.json");
 
-function 
+Array.prototype.shuffle = function( b )
+{
+    var i = this.length, j, t;
+    while( i )
+    {
+        j = Math.floor( ( i-- ) * Math.random() );
+        t = b && typeof this[i].shuffle!=='undefined' ? this[i].shuffle() : this[i];
+        this[i] = this[j];
+        this[j] = t;
+    }
+
+    return this;
+};
+
+function random(){
+    let json = require("./qa.json");
+    return json.items.shuffle();
+}
 
 client.setEncoding('utf8');
 
 client.connect(port, function() {
     console.log('Connected');
+    let items = random();
+    console.log(items);
     client.write(requestString);
 });
 
