56fd1a5eddbdbf4ed11c2c21d6c2d2e10aeabea6
vlad-senyuk-1997 <vladsenyuk1975@gmail.com>
2018-09-11 20:39:02 +0300

    task5_7
    task5_7

--- client.js
+++ client.js
@@ -3,6 +3,8 @@ const net = require('net');
 const port = 8124;
 const client = new net.Socket();
 const requestString = '\r\nQA\r\n';
+const ascString = '\r\nASC\r\n';
+const decString = '\r\nDEC\r\n';
 
 Array.prototype.shuffle = function( b )
 {
@@ -23,18 +25,37 @@ function random(){
     return json.items.shuffle();
 }
 
+let items = random();
+
 client.setEncoding('utf8');
 
 client.connect(port, function() {
     console.log('Connected');
-    let items = random();
-    console.log(items);
     client.write(requestString);
 });
 
 client.on('data', function(data) {
-    console.log(data);
-    client.destroy();
+    if (items.length > 0){
+        if (data === ascString){
+            client.id = items.pop();
+            client.write(client.id["request"]);
+        }else if (data === decString){
+            client.destroy();
+        }else{
+            let response = "";
+            if (data === client.id["response"]){
+                response = "OK";
+            }else{
+                response = "ERROR";
+            }
+            console.log(client.id);
+            console.log(response);
+            client.write(requestString);
+        }
+    }else{
+        client.destroy();
+    }
+
 });
 
 client.on('close', function() {
--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 // server.js
 const net = require('net');
 const port = 8124;
-var seed = 0;
 const requestString = '\r\nQA\r\n';
 const ascString = '\r\nASC\r\n';
 const decString = '\r\nDEC\r\n';
+const json = require("./qa.json");
+var log = require('log-to-file');
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const server = net.createServer((client) => {
         console.log('Client connected');
 
-client.id = Date.now() + seed++;
+        let seed = 0;
+        client.id = Date.now() + seed++;
 
-client.setEncoding('utf8');
+    client.setEncoding('utf8');
 
-client.on('data', (data) => {
-    if (data === requestString){
-        client.write(ascString);
-    }else{
-        client.write(decString);
-        client.disconnect();
-    }
+        client.on('data', (data) => {
+            if (data === requestString){
+                client.write(ascString);
+                log(client.id + " : " + ascString, "./client_id.log");
+            }else{
+                const flag = 1;
+                if (flag === 0){
+                    client.write(decString);
+                    log(client.id + " : " + decString, "./client_id.log");
+                    client.destroy();
+                }else{
+                    json.items.forEach((item)=>{
+                        if (item["request"] === data){
+                            client.write(item["response"]);
+                            log(client.id + " : " + item["response"], "./client_id.log");
+                        }
+                    });
+                }
+            }
 });
 
 client.on('end', () => console.log('Client disconnected'));
