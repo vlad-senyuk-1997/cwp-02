e8408b4ba917d00e62ab86a43b7236e3d756f183
vlad-senyuk-1997 <vladsenyuk1975@gmail.com>
2018-09-11 17:40:32 +0300

    adding id
    adding id

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 // server.js
 const net = require('net');
 const port = 8124;
+var seed = 0;
 
 const server = net.createServer((client) => {
         console.log('Client connected');
+
+client.id = Date.now() + seed++;
 
 client.setEncoding('utf8');
 
 client.on('data', (data) => {
     console.log(data);
-client.write('\r\nHello!\r\nRegards,\r\nServer\r\n');
+client.write('\r\nHello!\r\nRegards,\r\nServer\r\n' + '\r\n' + client.id);
 });
 
 client.on('end', () => console.log('Client disconnected'));
